cf783876c96dd12289469ef3bc456ac55b14e123
IceliosGit <119666701+IceliosGit@users.noreply.github.com>
2024-10-23 13:36:29 +0200

    end of day 25

--- day25/states_game/main.py
+++ day25/states_game/main.py
@@ -7,12 +7,28 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state name?")
-print(answer_state.capitalize())  # Put the first letter in uppercase if not already the case
+writer = turtle.Turtle()
+writer.hideturtle()
+writer.penup()
 
 data = pandas.read_csv("50_states.csv")
-state_name = data["state"]
-print(state_name)
+state_list = data["state"].to_list()
+number_guessed = 0
+
+while number_guessed < 50:
+    answer_state = screen.textinput(title=f"Guess the state  {number_guessed}/50", prompt="What's another state name?")
+    answer_state = answer_state.title()  # Put the first letter in uppercase if not already the case
+    # title() will put every single word with an uppercase first. capitalized will only do the first word
+    if answer_state in state_list:
+        number_guessed += 1
+        state_list.remove(answer_state)
+        state_coor = data[data.state == answer_state]
+        index = int(state_coor.index[0])  # Don't know why I have to do that, needed for below otherwise do not work
+
+        # prof uses writer.goto(state_data.x.item(), state_data.y.item()) instead to avoid this problem
+        writer.goto(state_coor.x[index], state_coor.y[index])
+        writer.write(f"{answer_state}", align="center", font=("Arial", 8))
+
 
 
 
