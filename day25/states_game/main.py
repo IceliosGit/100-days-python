25ebd7ae152e35c246105a3dfe222c62703ff89c
IceliosGit <119666701+IceliosGit@users.noreply.github.com>
2024-10-23 13:58:46 +0200

    added the creation of a csv with missing states when using exit

--- day25/states_game/main.py
+++ day25/states_game/main.py
@@ -19,6 +19,12 @@ while number_guessed < 50:
     answer_state = screen.textinput(title=f"Guess the state  {number_guessed}/50", prompt="What's another state name?")
     answer_state = answer_state.title()  # Put the first letter in uppercase if not already the case
     # title() will put every single word with an uppercase first. capitalized will only do the first word
+    if answer_state == "Exit":
+
+        df = pandas.DataFrame(state_list)
+        df.to_csv("List_of_missed_states.csv")
+        break
+
     if answer_state in state_list:
         number_guessed += 1
         state_list.remove(answer_state)
@@ -28,9 +34,3 @@ while number_guessed < 50:
         # prof uses writer.goto(state_data.x.item(), state_data.y.item()) instead to avoid this problem
         writer.goto(state_coor.x[index], state_coor.y[index])
         writer.write(f"{answer_state}", align="center", font=("Arial", 8))
-
-
-
-
-
-screen.exitonclick()
